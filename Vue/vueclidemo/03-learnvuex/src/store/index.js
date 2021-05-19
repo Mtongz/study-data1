9932eb1017fac6db7d6d7a74c18758061cf6eb49
Tong <915712823@qq.com>
2021-05-19 17:33:54 +0800

    add vuex

--- Vue/vueclidemo/03-learnvuex/src/store/index.js
+++ Vue/vueclidemo/03-learnvuex/src/store/index.js
@@ -1,28 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import getters from './getters'
+import actions from './actions'
+import mutations from './mutations'
+import modules from './modules'
+
 Vue.use(Vuex)
-
+const state = {
+  counter: 0
+}
 const store = new Vuex.Store({
-  state: {
-    counter: 0
-  },
-  mutations: {
-    increment(state) {
-      state.counter++
-    },
-    decrement(state) {
-      state.counter--
-    }
-  },
-  actions: {
-
-  },
-  getters: {
-
-  },
+  state,
+  mutations,
+  actions,
+  getters,
   modules: {
-
+    a: modules.moduleA
   }
 })
 
---
+++ Vue/vueclidemo/03-learnvuex/src/store/mutations.js
@@ -0,0 +1,8 @@
+export default {
+  increment(state) {
+    state.counter++
+  },
+  decrement(state) {
+    state.counter--
+  }
+}
